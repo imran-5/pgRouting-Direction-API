6b5ef9eda02b903540eedf8775eef4926d5012eb
Muhammad Imran Siddique <newton.imran@gamil.com>
2019-09-14 19:57:00 +0400

    'id_update'

--- src/model/pgRouting.js
+++ src/model/pgRouting.js
@@ -2,8 +2,9 @@ const {pool, query} = require('../config/database');
 const {config_pg} = require('../config/config');
 
 function distanceQuery(start, end) {
+    // ST_GeomFromText('POINT(24.240194 60.008345)', 4326)
     const query = `SELECT * FROM pgr_dijkstra(
-        'SELECT gid as id, source, target, cost_len as cost FROM ${config_pg.table}', 
+        'SELECT id, source, target, cost_len as cost FROM ${config_pg.table}', 
         (SELECT id FROM ${config_pg.vertices_table} ORDER BY st_distance(the_geom, st_setsrid(st_makepoint(${start}), ${config_pg.input_srid})) LIMIT 1), 
         (SELECT id FROM ${config_pg.vertices_table} ORDER BY st_distance(the_geom, st_setsrid(st_makepoint(${end}), ${config_pg.input_srid})) LIMIT 1)
         , false)`;
@@ -13,10 +14,10 @@ function distanceQuery(start, end) {
 function routeQuery(start, end) {
     const query = `
     SELECT *, st_transform(geom, ${config_pg.output_srid}) as geom, st_asgeojson(st_transform(geom, ${config_pg.output_srid})) geojson, st_astext(st_transform(geom, ${config_pg.output_srid})) wkt FROM pgr_dijkstra(
-        'SELECT gid as id, source, target, cost_len as cost, rcost_len as reverse_cost FROM ${config_pg.table}', 
+        'SELECT id, source, target, cost_len as cost, rcost_len as reverse_cost FROM ${config_pg.table}', 
         (SELECT id FROM ${config_pg.vertices_table} ORDER BY st_distance(the_geom, st_setsrid(st_makepoint(${start}), ${config_pg.input_srid})) LIMIT 1),
         (SELECT id FROM ${config_pg.vertices_table} ORDER BY st_distance(the_geom, st_setsrid(st_makepoint(${end}), ${config_pg.input_srid})) LIMIT 1),
-        false) as dj, ${config_pg.table} as ln where dj.edge=ln."gid";`;
+        false) as dj, ${config_pg.table} as ln where dj.edge=ln."id";`;
     return query;
 }
 
